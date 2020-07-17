67d4972ec8f6265aaf6c5ca9f53f7d123b55f3e7
SkylarLarsen <62402418+SkylarLarsen@users.noreply.github.com>
2020-07-16 19:18:50 -0700

    Update Varda_ECM_Test_Case.py

--- Varda_ECM_Test_Case.py
+++ Varda_ECM_Test_Case.py
@@ -1,29 +1,26 @@
 import math as m
 
-#mean + aSin1 Cos[(2 Pi (c - refCoor))/period] + bCos1 Cos[(4 Pi (c - refCoor))/period] + 
-#aSin1 Sin[(2 Pi (c - refCoor))/period] + bCos1 Sin[(4 Pi (c - refCoor))/period] + slope (c - refCoor)
-
 #THE FOLLOWING COORDINATES ARE FOR RA
-refCoor = 57371.3
-mean = 0.00812197
-slope = -0.0000123731
+refCoor = 57368.84735444445
+mean = 0.00806947
+slope = 0.00000856959
 period = 365.256
-aSin1 = 0.00403133
-bCos1 = -0.000433795
-c = 58369.14741055555
+aSin1 = 0.000588361
+bCos1 = 0.0000500055
+c = 58209
 
-RA_ECM_output = mean + aSin1 * m.cos((2 * m.pi * (c - refCoor))/period) + bCos1 * m.cos((4 * m.pi * (c - refCoor))/period) + aSin1 * m.sin((2 * m.pi * (c - refCoor))/period) + bCos1 * m.sin((4 * m.pi * (c - refCoor))/period) + slope * (c - refCoor)
+RA_ECM_output = mean + bCos1 * m.cos((2 * m.pi * (c - refCoor))/period) + aSin1 * m.sin((2 * m.pi * (c - refCoor))/period) + slope * (c - refCoor)
 print(RA_ECM_output)
 
 #THE FOLLOWING COORDINATES ARE FOR DEC
-refCoor = 57371.3
-mean = 0.0646109
-slope = 0.0000391309
+refCoor = 57368.84735444445
+mean = 0.0366658
+slope = 0.0000184514
 period = 365.256
-aSin1 = 0.0143764
-bCos1 = -0.00787495
-c = 58369.14741055555
+aSin1 = 0.00596366
+bCos1 = -0.00600029
+c = 58209
 
-Dec_ECM_output = mean + aSin1 * m.cos((2 * m.pi * (c - refCoor))/period) + bCos1 * m.cos((4 * m.pi * (c - refCoor))/period) + aSin1 * m.sin((2 * m.pi * (c - refCoor))/period) + bCos1 * m.sin((4 * m.pi * (c - refCoor))/period) + slope * (c - refCoor)
+Dec_ECM_output = mean + bCos1 * m.cos((2 * m.pi * (c - refCoor))/period) + aSin1 * m.sin((2 * m.pi * (c - refCoor))/period) + slope * (c - refCoor)
 print(Dec_ECM_output)
 
